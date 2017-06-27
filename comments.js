30e6837c407e9fbbaa5a935276c8805de21e7ab7
Tobiasz12 <tobiasz63@op.pl>
2017-06-27 12:13:14 +0200

    task 7.4

--- comments.js
+++ comments.js
@@ -1,4 +1,10 @@
-function comments(state = [], action) {
+import {ADD_COMMENT} from './actions'
+import {REMOVE_COMMENT} from './actions'
+import {EDIT_COMMENT} from './actions'
+import {THUMB_UP_COMMENT} from './actions'
+import {THUMB_DOWN_COMMENT} from './actions'
+
+ function comments(state = [], action) {
     switch(action.type) {
         case ADD_COMMENT:
             return [{
@@ -6,5 +12,26 @@ function comments(state = [], action) {
                 text: action.text,
                 votes: 0
             }, ...state.comments];
+        case THUMB_UP_COMMENT:
+            return state.votes + 1
+        case THUMB_DOWN_COMMENT:
+            return state.votes - 1
+        case REMOVE_COMMENT:
+            return Object.assign({},state,{
+                comments: state.comments.filter(comment => comment.id !== action.id)
+            });
+        case EDIT_COMMENT:
+            return Object.assign({},state,{
+                comments: state.comments.filter(comment => {
+                if (comment.id !== action.id) {
+                    return comment;
+                }
+                return object.assign({}, comment, {
+                    text: action.text
+                })
+            });
+        default:
     }
 }
+
+export default comments;
